6330b79d64d61dfd2dea9f030d7b8af3132f50d4
Vidy Videni <vidy.videni@gmail.com>
2019-06-27 11:41:00 +0800

    make sure ssh private key is added to ssh-agent

--- run.sh
+++ run.sh
@@ -9,6 +9,10 @@ usermod -u ${DOCKER_UID} www-data 2> /dev/null && {
     groupmod -g ${DOCKER_GID} www-data 2> /dev/null || usermod -a -G ${DOCKER_GID} www-data
 }
 
+# make sure ssh private key is added to ssh-agent
+eval `ssh-agent -s`
+ssh-add /root/.ssh/git
+
 chmod -R 777 /tmp
 
 exec supervisord -n
